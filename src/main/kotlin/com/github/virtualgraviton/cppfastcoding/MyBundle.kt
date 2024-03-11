0760432b3bdcd8dedb523fa472146b1009f95fbb
GitHub Action <action@github.com>
2024-03-11 14:11:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/virtualgraviton/cppfastcoding/MyBundle.kt
+++ src/main/kotlin/com/github/virtualgraviton/cppfastcoding/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.virtualgraviton.cppfastcoding
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/virtualgraviton/cppfastcoding/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/virtualgraviton/cppfastcoding/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.virtualgraviton.cppfastcoding.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
