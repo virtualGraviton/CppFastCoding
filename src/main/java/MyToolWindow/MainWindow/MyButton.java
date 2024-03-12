cdea41e9fc7591d842f13c85fefd8c78228f44e2
虚引力子 <3247868904@qq.com>
2024-03-12 13:50:21 +0800

    v1.0.0,重构

--- src/main/java/MyToolWindow/MainWindow/MyButton.java
+++ src/main/java/MyToolWindow/MainWindow/MyButton.java
@@ -2,17 +2,18 @@ package MyToolWindow.MainWindow;
 
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.FontPreferences;
-import com.intellij.ui.JBColor;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MyButton extends JButton {
 
-    MyButton(String name) {
-        super(name);
-        this.setBackground(JBColor.gray);
-        this.setContentAreaFilled(false);
+    MyButton(String Text) {
+        super(Text);
+        SetFont();
+    }
+
+    public void SetFont() {
         FontPreferences fontPreferences = EditorColorsManager.getInstance().getGlobalScheme().getFontPreferences();
         String fontType = fontPreferences.getFontFamily();
         int fontSize = fontPreferences.getSize(fontType);
