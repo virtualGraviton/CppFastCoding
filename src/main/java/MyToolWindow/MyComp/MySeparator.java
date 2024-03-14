8a19373c19545675b5ae23a66d0ff7da294b4a04
虚引力子 <3247868904@qq.com>
2024-03-14 16:28:40 +0800

    v1.0.0

---
+++ src/main/java/MyToolWindow/MyComp/MySeparator.java
@@ -0,0 +1,10 @@
+package MyToolWindow.MyComp;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MySeparator extends JSeparator {
+    public MySeparator(){
+        this.setMaximumSize(new Dimension(2000, 10));
+    }
+}
