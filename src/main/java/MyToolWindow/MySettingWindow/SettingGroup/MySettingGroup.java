bf42ccb8dc700db630cda47119f9f015f19e46ef
虚引力子 <3247868904@qq.com>
2024-03-15 19:19:54 +0800

    v1.0.0

--- src/main/java/MyToolWindow/MySettingWindow/SettingGroup/MySettingGroup.java
+++ src/main/java/MyToolWindow/MySettingWindow/SettingGroup/MySettingGroup.java
@@ -2,7 +2,6 @@ package MyToolWindow.MySettingWindow.SettingGroup;
 
 import MyToolWindow.MyComp.MyLabel;
 import MyToolWindow.MyComp.MyPanel;
-import MyToolWindow.MyComp.MySeparator;
 import MyToolWindow.MyComp.MyTextArea;
 import com.intellij.ui.JBColor;
 
@@ -11,7 +10,7 @@ import javax.swing.*;
 public class MySettingGroup extends MyPanel {
     public MySettingGroup(String groupName) {
         super(BoxLayout.Y_AXIS);
-        this.AddComp( new MyLabel(groupName));
+        this.AddComp(new MyLabel(groupName));
         this.AddComp(new MyTextArea());
         this.setBackground(JBColor.green);
     }
