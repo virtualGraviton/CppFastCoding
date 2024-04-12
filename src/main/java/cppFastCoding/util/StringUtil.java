b99970275f53d6e64c55c36b52f9238003827c62
虚引力子 <3247868904@qq.com>
2024-04-12 21:08:23 +0800

    v1.0.3

--- src/main/java/cppFastCoding/util/StringUtil.java
+++ src/main/java/cppFastCoding/util/StringUtil.java
@@ -3,9 +3,9 @@ package cppFastCoding.util;
 import java.util.ArrayList;
 
 public class StringUtil {
-    public static boolean isEqual(String a, String b) {
-        ArrayList<String> l1 = split(a);
-        ArrayList<String> l2 = split(b);
+    public static boolean isEqual(String s1, String s2) {
+        ArrayList<String> l1 = split(s1);
+        ArrayList<String> l2 = split(s2);
         if (l1.size() != l2.size()) return false;
         for (int i = 0; i < l1.size(); i++) {
             if (!l1.get(i).equals(l2.get(i))) return false;
